fe5c11f2f0da410166986e96675c8f5e4bde546f
areksu <alexlindfors93@gmail.com>
2019-06-11 09:40:44 +0900

    Add local data to create more interesting currency list, add filters on new data

--- src/Currency.js
+++ src/Currency.js
@@ -1,11 +1,29 @@
 export default class Currency {
-    constructor(name,rate) {
+    constructor(symbol,name,rate,country) {
+        this.symbol = symbol;
         this.name = name;
-        this.rate = rate
+        this.rate = rate;
+        this.country = country;
     }
 
     exchange(amount, other) {
         let result = (amount / this.rate) * other.rate
         return result
+    }
+
+    contains(filter) {
+
+        if (this.symbol.includes(filter.toUpperCase())) {
+            return true
+        }
+        if (this.country.toLowerCase().includes(filter.toLowerCase())) {
+            return true
+        }
+
+        if (this.name.toLowerCase().includes(filter.toLowerCase())) {
+            return true
+        }
+
+        return false
     }
 }
